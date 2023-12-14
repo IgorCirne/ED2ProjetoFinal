43c605571b5bd5531f3006539417ff875cebce63
Igor Cirne <igor.oliveira.100@ufrn.edu.br>
2023-12-14 17:15:25 -0300

    Alterações feitas
    Replicado o processo de criação do grafo para os três grafos gerados

--- Análise do Grafo/Funções.py
+++ Análise do Grafo/Funções.py
@@ -1,9 +1,20 @@
-def diferenca_grafos(grafo1, grafo2):
+import networkx as nx
+import osmnx as ox
+import matplotlib
+
+
+def diff_grafos(grafo1, grafo2):
     # Cria uma cópia dos grafos originais para evitar alterações indesejadas
-    diferenca = grafo1.copy()
+    difff = grafo1.copy()
 
     # Remove os nós e arestas que estão presentes no segundo grafo
-    diferenca.remove_nodes_from(grafo2.nodes())
-    diferenca.remove_edges_from(grafo2.edges())
+    difff.remove_nodes_from(grafo2.nodes())
+    difff.remove_edges_from(grafo2.edges())
 
-    return diferenca
+    return difff
+
+def centralidade(A):
+    bc = nx.betweenness_centrality(ox.get_digraph(A), weight="length")
+    max_node, max_bc = max(bc.items(), key=lambda x: x[1])
+    max_node, max_bc
+    nx.set_node_attributes(A, bc, "bc")
--- Análise do Grafo/main.py
+++ Análise do Grafo/main.py
@@ -4,28 +4,65 @@ import matplotlib
 import Funções
 
 
-
-
-#B = ox.graph_from_place("Pipa, Tibau do Sul, Brazil", network_type="drive")
-G = ox.graph_from_place("Natal, Brazil", network_type="bike")
-#grafo_diferenca = Funções.diferenca_grafos(G, B)
+B = ox.graph_from_place("Pipa, Tibau do Sul, Brazil", network_type="drive")
+G = ox.graph_from_place("Pipa, Tibau do Sul, Brazil", network_type="bike")
+grafo_diff = Funções.diff_grafos(G, B)
 
 # calculos para centralidade
 
-bc = nx.betweenness_centrality(ox.get_digraph(G), weight="length")
-max_node, max_bc = max(bc.items(), key=lambda x: x[1])
-max_node, max_bc
-nx.set_node_attributes(G, bc, "bc")
-nc = ox.plot.get_node_colors_by_attr(G, "bc", cmap="plasma")
+Funções.centralidade(G)
+Funções.centralidade(B)
+Funções.centralidade(grafo_diff)
 
+# Outros atributos da rede
+
+# Número de nós
+num_nodes = len(G.nodes)
+print(f"Número de nós: {num_nodes}")
+
+# Número de arestas
+num_edges = len(G.edges)
+print(f"Número de arestas: {num_edges}")
+
+# Densidade da rede
+density = nx.density(G)
+print(f"Densidade da rede: {density}")
+
+# Comprimento total das arestas
+
+total_edge_length = sum(nx.get_edge_attributes(G, "length").values())
+print(f"Comprimento total das arestas: {total_edge_length} metros")
+
+# Desenhando o grafo
+
+ncg = ox.plot.get_node_colors_by_attr(G, "bc", cmap="plasma")
+ncb = ox.plot.get_node_colors_by_attr(B, "bc", cmap="plasma")
+ncd = ox.plot.get_node_colors_by_attr(grafo_diff, "bc", cmap="plasma")
 
 fig, ax = ox.plot_graph(
     G,
-    node_color=nc,
+    node_color=ncg,
     node_size=30,
     node_zorder=2,
     edge_linewidth=0.2,
     edge_color="w",
 )
 
-#fig, ax = ox.plot_graph(G)
+fig, ax = ox.plot_graph(
+    B,
+    node_color=ncb,
+    node_size=30,
+    node_zorder=2,
+    edge_linewidth=0.2,
+    edge_color="w",
+)
+
+fig, ax = ox.plot_graph(
+    grafo_diff,
+    node_color=ncd,
+    node_size=30,
+    node_zorder=2,
+    edge_linewidth=0.2,
+    edge_color="w",
+)
+
